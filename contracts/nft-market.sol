638b7a0fd82a56589ff4ba785d9c729001c5f3ce
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2023-12-03 19:41:24 +0800

    update nft market

--- contracts/nft-market.sol
+++ contracts/nft-market.sol
@@ -8,6 +8,8 @@ contract Market {
     IERC20 public erc20;
     IERC721 public erc721;
 
+    bytes4 internal constant MAGIC_ON_ERC721_RECEIVED = 0x150b7a02;
+
     struct Order {
         address seller;
         uint256 tokenId;
@@ -46,6 +48,81 @@ contract Market {
         require(erc20.transferFrom(buyer, seller, price), "transfer failed");
         erc721.safeTransferFrom(address(this), buyer, _tokenId);
 
+        //  remove order
+        removeOrder(_tokenId);
         emit Deal(seller, buyer, _tokenId, price);
+    }
+
+    function cancelOrder(uint256 _tokenId) external {
+        address seller = orderOfId[_tokenId].seller;
+        require(seller == msg.sender, "not the seller");
+        erc721.safeTransferFrom(address(this), seller, _tokenId);
+        //  remove order
+        removeOrder(_tokenId);
+        emit OrderCancelled(seller, _tokenId);
+    }
+
+    function changePrice(uint256 _tokenId, uint256 _price) external {
+        address seller = orderOfId[_tokenId].seller;
+        require(seller == msg.sender, "not the seller");
+        require(_price > 0, "price is zero");
+        uint256 previousPrice = orderOfId[_tokenId].price;
+        orderOfId[_tokenId].price = _price;
+
+        Order storage order = orders[idToOrderIndex[_tokenId]];
+        order.price = _price;
+        emit PriceChanged(seller, _tokenId, previousPrice, _price);
+    }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4) {
+        require(msg.sender == address(erc721), "invalid sender");
+        require(
+            erc721.ownerOf(tokenId) == address(this),
+            "not owned by this contract"
+        );
+        uint256 price = toUint256(data, 0);
+        require(price > 0, "price is zero");
+        // 上架
+        orders.push(Order(from, tokenId, price));
+        orderOfId[tokenId] = Order(from, tokenId, price);
+        idToOrderIndex[tokenId] = orders.length - 1;
+        emit NewOrder(from, tokenId, price);
+
+        return MAGIC_ON_ERC721_RECEIVED;
+    }
+
+    function removeOrder(uint256 _tokenId) internal {
+        // 把index 和 最后一个元素进行调换，然后把index pop出去
+        uint256 index = idToOrderIndex[_tokenId];
+        uint256 lastIndex = orders.length - 1;
+        if (index != lastIndex) {
+            Order storage lastOrder = orders[lastIndex];
+            idToOrderIndex[lastOrder.tokenId] = index;
+            orders[index] = lastOrder;
+        }
+        orders.pop();
+        delete orderOfId[_tokenId];
+        delete idToOrderIndex[_tokenId];
+    }
+
+    // https://stackoverflow.com/questions/63252057/how-to-use-bytestouint-function-in-solidity-the-one-with-assembly
+    function toUint256(
+        bytes memory _bytes,
+        uint256 _start
+    ) public pure returns (uint256) {
+        require(_start + 32 >= _start, "Market: toUint256_overflow");
+        require(_bytes.length >= _start + 32, "Market: toUint256_outOfBounds");
+        uint256 tempUint;
+
+        assembly {
+            tempUint := mload(add(add(_bytes, 0x20), _start))
+        }
+
+        return tempUint;
     }
 }
